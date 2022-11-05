42d726bb517aeacd1b0bc2cc90ca98b623e7f242
Anna Lou Parejo <aparejo0984@gmail.com>
2022-11-05 13:29:26 -0400

    User Locations Changes

--- app/Models/UserLocation.php
+++ app/Models/UserLocation.php
@@ -10,4 +10,19 @@ class UserLocation extends Model
     use HasFactory;
 
     public $table = 'user_locations';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'address1',
+        'address2',
+        'city',
+        'state',
+        'postal_code',
+        'country'
+    ];
 }
